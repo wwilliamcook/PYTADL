f8af77178a9684c85668f13a45138f8809efdc5d
Weston Cook <wwilliamcook@gmail.com>
2021-03-14 06:44:43 -0700

    Remove unused imports and update docstrings

--- pytadl/core.py
+++ pytadl/core.py
@@ -1,22 +1,33 @@
 import ffmpeg
-import io
 import numpy as np
 import os
 from pytube import YouTube
 
 
-def downloadMP4(youtube_url):
+def downloadMP4(youtube_url, output_path=None, filename=None):
+    """Downloads an MP4 of the given YouTube video.
+
+    :param str: youtube_url: URL of YouTube video to download
+    :param str: output_path: directory to save file
+    :param str: filename: base name to save file as
+    :return: the path to the saved file
+    """
     return (
         YouTube(youtube_url)
         .streams
         .filter(file_extension='mp4')
         .first()
-        .download(filename='youtube-dl_temp')
+        .download(output_path=output_path, filename=filename)
     )
 
 
 def downloadAndDecode(youtube_url):
-    filename = downloadMP4(youtube_url)
+    """Returns the audio data for the given YouTube video.
+
+    :param str: youtube_url: URL of YouTube video to download
+    :return: the mono audio samples as an int16 numpy array
+    """
+    filename = downloadMP4(youtube_url, filename='pytadl-temp')
     buffer, _ = (
         ffmpeg
         .input(filename)
